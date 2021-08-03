8434a12f35f115cf2c42f5010a7e6a96980e7c0e
Kevin <kevinwang0324@gmail.com>
2021-08-02 21:13:21 -0700

    reorganized and created skeleton code for obj

---
+++ src/json_obj.py
@@ -0,0 +1,15 @@
+
+class json_obj:
+    def __init__(self, filename = "", ):
+        self.filename = filename
+
+
+    def load_file(self):
+
+    def coco_to_supervisely(self):
+        supervisely_json = {}
+
+    def supervisely_to_coco(self):
+        coco_json = {}
+
+
